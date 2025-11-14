0340bf08bcc57c077cecc480ea1e0ab0e2665ab9
devalirehman <alyrehman07@gmail.com>
2025-11-13 18:44:41 -0800

    Frontend completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-     
+      theme: ThemeData(primarySwatch: Colors.blue),
+
       home: TicTac(),
     );
   }
 }
-
 
 class TicTac extends StatefulWidget {
   const TicTac({super.key});
@@ -27,13 +27,42 @@ class TicTac extends StatefulWidget {
 }
 
 class _TicTacState extends State<TicTac> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('TicTac'),backgroundColor: Colors.blue,),
+      appBar: AppBar(title: Text('TicTac'), backgroundColor: Colors.blue),
+      body: Column(
+        children: [
+          Text('Game State', style: const TextStyle(fontSize: 30)),
+
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                return GridTile(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: MaterialButton(onPressed: () {}, child: Text('X')),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          MaterialButton(
+              onPressed: (){},
+            child: const Text('Restart Game'),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
-
